b3d28de84de4bfa3c912af2ac242e2be0272c08b
Oktawian Kausz <play26400@gmail.com>
2025-04-03 17:15:10 +0200

    Increase version

--- utils/saveData.ts
+++ utils/saveData.ts
@@ -18,15 +18,13 @@ export const exportData = async () => {
       { text: 'Zapisz do Pobranych', onPress: () => saveToDownloads(fileName) },
       { text: 'Anuluj', style: 'cancel' }
     ]
-  )
+  );
 }
 
 const getAllAsyncStorageData = async () => {
   try {
-    const keys = await AsyncStorage.getAllKeys()
-    const data = await AsyncStorage.multiGet(keys)
-
-    console.log('Dane: ', data)
+    const keys = await AsyncStorage.getAllKeys();
+    const data = await AsyncStorage.multiGet(keys);
 
     const parsedData = Object.fromEntries(
       data.map(([key, value]) => {
@@ -36,32 +34,30 @@ const getAllAsyncStorageData = async () => {
           return [key, value]
         }
       })
-    )
+    );
 
-    return JSON.stringify(parsedData, null, 2)
+    return JSON.stringify(parsedData, null, 2);
   } catch (error) {
-    console.error('Błąd podczas pobierania danych:', error)
-    return '{}'
+    console.error('Błąd podczas pobierania danych:', error);
+    return '{}';
   }
 }
 
 const saveToDownloads = async (fileName: string) => {
   if (Platform.OS === 'android') {
-    const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync()
+    const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
     if (!permissions.granted) {
-      console.warn('Brak uprawnień do zapisu w Downloads')
-      return
+      console.warn('Brak uprawnień do zapisu w Downloads');
+      return;
     }
 
     const uri = await FileSystem.StorageAccessFramework.createFileAsync(
       permissions.directoryUri,
       fileName,
       'application/json'
-    )
+    );
 
     const json = await getAllAsyncStorageData();
     await FileSystem.writeAsStringAsync(uri, json, { encoding: FileSystem.EncodingType.UTF8 });
-
-    console.log('Plik zapisany w Pobranych!')
   }
 }

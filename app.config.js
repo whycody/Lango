2e9585d74adfda8d90362bef92539eda9552dc26
Oktawian Kausz <play26400@gmail.com>
2025-11-29 09:53:00 +0100

    Add google-services configuration files

--- app.config.js
+++ app.config.js
@@ -7,7 +7,13 @@ export default ({config}) => {
     name: isTest ? "LangoTest" : isDev ? "LangoDev" : "Lango",
     android: {
       ...config.android,
-      package: isTest ? "com.whycody.lango.test" : isDev ? 'com.whycody.lango.dev' : "com.whycody.lango"
+      package: isTest ? "com.whycody.lango.test" : isDev ? 'com.whycody.lango.dev' : "com.whycody.lango",
+      googleServicesFile: isTest ? "./config/google-services-test.json" : "./config/google-services.json"
+    },
+    ios: {
+      ...config.ios,
+      bundleIdentifier: isTest ? "com.whycody.lango.test" : "com.whycody.lango",
+      googleServicesFile: isTest ? "./config/GoogleService-Info-Test.plist" : "./config/GoogleService-Info.plist"
     }
   };
 };

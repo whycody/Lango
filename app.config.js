a4851346d10829e1a27e99d29cadcd3c46fc97a8
Oktawian Kausz <play26400@gmail.com>
2026-02-05 23:35:51 +0100

    Fix env variables

--- app.config.js
+++ app.config.js
@@ -8,12 +8,15 @@ export default ({config}) => {
     android: {
       ...config.android,
       package: isTest ? "com.whycody.lango.test" : isDev ? "com.whycody.lango.dev" : "com.whycody.lango",
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON
+      googleServicesFile: (process.env.GOOGLE_SERVICES_JSON ?? "config/google-services.json")
     },
     ios: {
       ...config.ios,
       bundleIdentifier: isTest ? "com.whycody.lango.test" : isDev ? "com.whycody.lango.dev" : "com.whycody.lango",
-      googleServicesFile: isTest ? process.env.GOOGLE_SERVICES_IOS_TEST : isDev ? process.env.GOOGLE_SERVICES_IOS_DEV : process.env.GOOGLE_SERVICES_IOS
+      googleServicesFile: isTest ?
+        (process.env.GOOGLE_SERVICES_IOS_TEST ?? "config/GoogleService-Info-Test.plist") : isDev ?
+          (process.env.GOOGLE_SERVICES_IOS_DEV ?? "config/GoogleService-Info-Dev.plist") :
+          (process.env.GOOGLE_SERVICES_IOS ?? "config/GoogleService-Info.plist")
     }
   };
 };

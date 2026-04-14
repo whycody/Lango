a03d1034d202973225832e23713118a242050089
Oktawian Kausz <play26400@gmail.com>
2026-04-14 02:14:39 +0200

    fix: remove unused import of react-native-config from apiHandler

--- src/api/apiHandler.ts
+++ src/api/apiHandler.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import * as Updates from 'expo-updates';
-import Config from 'react-native-config';
 
 import { createAuthData } from '../utils/authUtils';
 
